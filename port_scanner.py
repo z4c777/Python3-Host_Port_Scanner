863874af9fce47dbdb8345c9abe2ad5c160ebb47
z4c777 <35697954+z4c777@users.noreply.github.com>
2021-06-09 13:37:59 -0500

    Updated port_scanner.py

--- port_scanner.py
+++ port_scanner.py
@@ -1,14 +1,12 @@
 #!/usr/bin/python3
- 
-import socket
- 
-ip = socket.gethostbyname (socket.gethostname())
- 
+
+from socket import gethostbyname, gethostname, AF_INET, SOCK_STREAM, socket
+
+HOST = gethostbyname(gethostname())
+
 for port in range(65535):
-    try:
-        serv = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-        serv.bind((ip,port))
-    except:
-        print('Port:',port,'is open')
- 
-    serv.close()
+	with socket(AF_INET, SOCK_STREAM) as sock:
+		try:
+			sock.bind((HOST, port))
+		except OSError:
+			print(f"Port: {port} is open")
